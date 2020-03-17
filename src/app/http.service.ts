cbe63fef0f6bb7b187a2b932ea649650733edff6
Víctor Peña Romero <vic.pero@icloud.com>
2020-03-17 15:37:12 -0600

    done

---
+++ src/app/http.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getBeer() {
+    return this.http.get('https://api.openbrewerydb.org/breweries')
+  }
+
+  myMethod() {
+    return console.log('Holi consoli')
+  }
+}
